017c16a516484a82d0d31effa8eed8074ddd9bde
Ryo Sugimoto <sugryo1109@gmail.com>
2016-06-10 19:00:33 +0900

    Add package "wget"

--- lib/itamae/plugin/recipe/h2o/dependency.rb
+++ lib/itamae/plugin/recipe/h2o/dependency.rb
@@ -1,7 +1,9 @@
 # cf) https://h2o.examp1e.net/install.html
 case node[:platform]
 when 'debian', 'ubuntu', 'mint'
+  package 'wget'
   package 'cmake'
 else
+  package 'wget'
   package 'cmake'
 end
