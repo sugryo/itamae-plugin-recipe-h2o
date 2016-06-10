8d1215e2826ff2cdfcc7a0f4b20aa8e2104c8085
Ryo Sugimoto <sugryo1109@gmail.com>
2016-06-10 19:34:22 +0900

    Fix a bug

--- lib/itamae/plugin/recipe/h2o/dependency.rb
+++ lib/itamae/plugin/recipe/h2o/dependency.rb
@@ -1,16 +1,17 @@
 # cf) https://h2o.examp1e.net/install.html
+p node[:h2o][:mruby]
 case node[:platform]
 when 'debian', 'ubuntu', 'mint'
   package 'wget'
   package 'cmake'
-  if node[:h2o][:mruby] == 'on'
+  if node[:h2o][:mruby]
     package 'bison'
     package 'ruby-dev'
   end
 else
   package 'wget'
   package 'cmake'
-  if node[:h2o][:mruby] == 'on'
+  if node[:h2o][:mruby]
     package 'bison'
     package 'ruby'
   end
