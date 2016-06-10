fd0c58c0dedaa7c12c486253928ff299776d49db
Ryo Sugimoto <sugryo1109@gmail.com>
2016-06-10 16:50:25 +0900

    Initial commit

---
+++ lib/itamae/plugin/recipe/h2o.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/h2o/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module H2o
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/recipe/h2o/version.rb
@@ -0,0 +1,9 @@
+module Itamae
+  module Plugin
+    module Recipe
+      module H2o
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
